d4692a276129c46234e6aba4c8a076b4324b2168
401A <timurbakibayev@gmail.com>
2018-11-22 15:51:03 +0600

    project init

---
+++ jupyterkz/fiesta/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FiestaConfig(AppConfig):
+    name = 'fiesta'
---
+++ jupyterkz/fiesta/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils.timezone import now as timezone_now
+import os
+
+# Create your models here.
+class Author(models.Model):
+    name = models.TextField(max_length=100, blank=False, null=False)
+    path = models.TextField(max_length=100, blank=False, null=False)
+
+
+def upload_to(instance, filename):
+    now = timezone_now()
+    filename_base, filename_ext = os.path.splitext(filename)
+    return 'my_uploads/{}_{}{}'.format(
+        now.strftime("%Y_%m_%d/%H%M%S"),
+        filename_base,
+        filename_ext.lower()
+    )
+
+
+class Attachment(models.Model):
+    parent_id = models.CharField(max_length=18)
+    file_name = models.CharField(max_length=255)
+    attachment = models.FileField(upload_to=upload_to)
+    user_id = models.IntegerField(default=-1)
+
+    def __str__(self):
+        return self.file_name
+
+    class Meta:
+        ordering = ["-file_name"]
