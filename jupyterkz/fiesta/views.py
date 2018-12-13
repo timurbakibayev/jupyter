d136a4bd7d25edd15a548ef042df2fac54d993e6
401a <timurbakibayev@gmail.com>
2018-12-13 16:28:02 +0600

    show name instead of 2 months ago

--- jupyterkz/fiesta/views.py
+++ jupyterkz/fiesta/views.py
@@ -118,7 +118,7 @@ def index(request, folder_name=""):
 
     if folder_name == "":
         files = []
-        dirs = [i.username for i in User.objects.all() if not i.is_superuser]
+        dirs = [{"folder": i.username, "name": i.first_name} for i in User.objects.all() if not i.is_superuser]
     else:
         by_user = get_object_or_404(User, username=folder_name.lower())
         files = Html.objects.filter(author=by_user)
